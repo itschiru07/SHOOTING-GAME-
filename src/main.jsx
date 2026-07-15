928f31cee33f858a17f27f1f7770f2598379e80c
itschiru07 <its.chiru07@gmail.com>
2026-07-15 11:36:30 +0530

    Fix: Resolve ReactDOM ReferenceError and load index.css in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './App.css'
+import './index.css'
 import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
     <App />
-  </React.StrictMode>,
+  </StrictMode>,
 )
