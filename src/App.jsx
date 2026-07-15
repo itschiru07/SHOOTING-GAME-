b655b19b17c81bafe2ad16283923cab11f33c157
itschiru07 <its.chiru07@gmail.com>
2026-07-15 10:39:45 +0530

    Fix: Resolve component import paths and capitalization in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-function app() {
-  return(
-    <div>
-      <h1>Space Shooter Lite</h1>
-    </div>
-  
-  )
-}
+import "./App.css";
+import Header from "./components/Header";
+import Gameover from "./components/Gameover";
+import GameBoard from "./components/GameBoard";
+import Scoreboard from "./components/scoreboard";
+import Control from "./components/Control";
 
-export default app;
+
+function app(){
+  return(
+    <div className="app">
+      <Header/>
+      <Scoreboard/>
+      <GameBoard/>
+      
+      <Control/>
+     
+    </div>
+  );
+}
+export default app
